e2c455cce9da1f6a07e3532a82a080a3ca182923
alyson-singleton <55365254+alyson-singleton@users.noreply.github.com>
2025-08-23 20:35:59 -0500

    translate to spanish

--- scratch/data_prep.R
+++ scratch/data_prep.R
@@ -13,7 +13,14 @@ library(readr)
 dengue_yearly <- readRDS("~/Desktop/doctorate/ch2 mdd highway/IOH_and_dengue_proj/data/clean/dengue_yearly_panels.rds")
 leish_yearly <- readRDS("~/Desktop/doctorate/ch2 mdd highway/IOH_and_dengue_proj/data/clean/leish_yearly_panels.rds")
 
-# Process dengue data
+# Process dengue data english
+dengue_tutorial_df_english <- dengue_yearly$connected_buffered %>%
+  mutate(year = substr(as.character(year), 1, 4)) %>%
+  select(year, key, incidence, fivekm, urban, ag, 
+         mean_temp_centered, mean_temp_centered_sq, 
+         sum_precip_centered)
+
+# Process dengue data spanish
 dengue_tutorial_df <- dengue_yearly$connected_buffered %>%
   mutate(year = substr(as.character(year), 1, 4)) %>%
   select(year, key, incidence, fivekm, urban, ag, 
@@ -30,6 +37,13 @@ dengue_tutorial_df <- dengue_yearly$connected_buffered %>%
     temp_cuad = mean_temp_centered_sq,
     precip = sum_precip_centered
   )
+
+# Process leish data english
+leish_tutorial_df_english <- leish_yearly$connected_buffered %>%
+  mutate(year = substr(as.character(year), 1, 4)) %>%
+  select(year, key, incidence, fivekm, urban, ag, 
+         mean_temp_centered, mean_temp_centered_sq, 
+         sum_precip_centered)
 
 # Process leish data
 leish_tutorial_df <- leish_yearly$connected_buffered %>%
@@ -51,4 +65,6 @@ leish_tutorial_df <- leish_yearly$connected_buffered %>%
 
 # Save data
 saveRDS(dengue_tutorial_df, file = "dengue_tutorial_df.rds")
+saveRDS(dengue_tutorial_df_english, file = "scratch/dengue_tutorial_df_english.rds")
 saveRDS(leish_tutorial_df, file = "leish_tutorial_df.rds")
+saveRDS(leish_tutorial_df_english, file = "scratch/leish_tutorial_df_english.rds")
